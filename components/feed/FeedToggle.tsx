010594ca2fb5d53bd0c0e3e3383fe8edaea52e6b
ankushh14 <ankushshenoy97@gmail.com>
2024-08-03 19:01:28 +0530

    feat : created feed toggle ui b/w recent and following

---
+++ components/feed/FeedToggle.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const FeedToggle = () => {
+  const pathname = usePathname();
+  return (
+    <div className="w-full flex p-3">
+      <Tabs
+        defaultValue={pathname.includes("following") ? "following" : "recent"}
+        className="w-full"
+      >
+        <TabsList className="w-full">
+          <Link href={"/feed"} className="w-full">
+            <TabsTrigger value="recent" className="w-full">
+              Recent
+            </TabsTrigger>
+          </Link>
+          <Link href={"/feed/following"} className="w-full">
+            <TabsTrigger value="following" className="w-full">
+              Following
+            </TabsTrigger>
+          </Link>
+        </TabsList>
+      </Tabs>
+    </div>
+  );
+};
+
+export default FeedToggle;
