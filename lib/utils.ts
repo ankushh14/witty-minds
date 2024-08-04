3c6fff7b0ba2f40d350c8f51a7adbe8d2096350f
ankushh14 <ankushshenoy97@gmail.com>
2024-08-04 17:27:42 +0530

    feat : added custom createToast function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { toast } from "@/components/ui/use-toast";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const createToast = ({
+  variant,
+  title,
+  description,
+}: {
+  variant: "default" | "destructive" | null | undefined;
+  title: string;
+  description?: string;
+}) => {
+  return toast({
+    "aria-atomic": "true",
+    role: "alert",
+    "aria-live": "assertive",
+    title,
+    variant,
+    description,
+  });
+};
