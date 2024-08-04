c877b086ec30352490cbf246abb267d9d885f410
ankushh14 <ankushshenoy97@gmail.com>
2024-08-05 02:12:23 +0530

    feat : loaders added to all pages

---
+++ app/(root)/feed/following/loading.tsx
@@ -0,0 +1,9 @@
+import Spinner from "@/components/ui/Spinner";
+
+export default function Feedloader() {
+  return (
+    <div className="w-full py-12 flex justify-center items-center">
+      <Spinner />
+    </div>
+  );
+}
