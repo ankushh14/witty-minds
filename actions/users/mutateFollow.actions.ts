98a996a6c0ecbf239a6484a09880f3157eafdd5c
ankushh14 <ankushshenoy97@gmail.com>
2024-08-05 02:10:51 +0530

    feat : mutate follow server action added

---
+++ actions/users/mutateFollow.actions.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import prisma from "@/prisma/prisma";
+import { ActionsReturnType, AddFollowProps, RemoveFollowProps } from "@/types";
+import { revalidateTag } from "next/cache";
+
+export const addFollow = async ({
+  fromUserID,
+  toUserID,
+}: AddFollowProps): Promise<ActionsReturnType> => {
+  try {
+    if (!fromUserID || !toUserID) {
+      return { message: "Bad Request", valid: false };
+    }
+    await prisma.user.update({
+      where: { id: fromUserID },
+      data: { following: { connect: { id: toUserID } } },
+    });
+    revalidateTag("posts");
+    return { message: "follow succesful!", valid: true };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Some unknown error occured",
+      valid: false,
+    };
+  }
+};
+
+export const removeFollow = async ({
+  fromUserID,
+  toUserID,
+}: RemoveFollowProps): Promise<ActionsReturnType> => {
+  try {
+    if (!fromUserID || !toUserID) {
+      return { message: "Bad Request", valid: false };
+    }
+    await prisma.user.update({
+      where: { id: fromUserID },
+      data: { following: { disconnect: { id: toUserID } } },
+    });
+    return { message: "unfollow succesful!", valid: true };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Some unknown error occured",
+      valid: false,
+    };
+  }
+};
