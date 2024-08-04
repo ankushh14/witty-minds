0136ddaba575cc6fa34671632df06f4e9840f029
ankushh14 <ankushshenoy97@gmail.com>
2024-08-05 02:08:08 +0530

    feat : mutate bookmarks action added

---
+++ actions/posts/mutateBookmarks.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import prisma from "@/prisma/prisma";
+import { ActionsReturnType, UpdateBookmarkProps } from "@/types";
+import { revalidateTag } from "next/cache";
+
+export const mutateBookmarks = async ({
+  postID,
+  userID,
+  addBookmark,
+}: UpdateBookmarkProps): Promise<ActionsReturnType> => {
+  try {
+    if (addBookmark) {
+      await prisma.post.update({
+        where: { id: postID },
+        data: { bookmarkedBy: { connect: { id: userID } } },
+      });
+      revalidateTag("posts");
+      return { message: "Bookmark added", valid: true };
+    } else {
+      await prisma.post.update({
+        where: { id: postID },
+        data: { bookmarkedBy: { disconnect: { id: userID } } },
+      });
+      revalidateTag("posts");
+      return {
+        message: "Bookmark removed",
+        valid: true,
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Bookmark update unsuccesful!",
+      valid: false,
+    };
+  }
+};
