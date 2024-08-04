157856aa45a4007d354d07b294017f488073b871
ankushh14 <ankushshenoy97@gmail.com>
2024-08-05 02:07:09 +0530

    feat : added getfollowing and getBookmarked posts server actions

--- actions/posts/getPosts.actions.ts
+++ actions/posts/getPosts.actions.ts
@@ -1,6 +1,8 @@
+"use server";
+
 import prisma from "@/prisma/prisma";
 import { ActionsReturnType } from "@/types";
-import { currentUser } from "@clerk/nextjs/server";
+import { unstable_cache } from "next/cache";
 
 type RecentPostsReturn = ActionsReturnType & {
   data?: {
@@ -44,72 +46,234 @@ type RecentPostsReturn = ActionsReturnType & {
   }[];
 };
 
-export const getRecentPosts = async (): Promise<RecentPostsReturn> => {
-  const clerkUser = await currentUser();
-  if (!clerkUser?.id) {
-    return { message: "Invalid User!", valid: false };
-  }
-  try {
-    const limit = 10;
-    const data = await prisma.post.findMany({
-      take: limit + 1,
-      orderBy: [
-        {
-          createdAt: "desc",
+export const getRecentPosts = unstable_cache(
+  async ({ id }: { id: string }): Promise<RecentPostsReturn> => {
+    try {
+      const limit = 10;
+      const data = await prisma.post.findMany({
+        take: limit + 1,
+        orderBy: [
+          {
+            createdAt: "desc",
+          },
+        ],
+        select: {
+          _count: true,
+          likedBy: true,
+          bookmarkedBy: true,
+          author: true,
+          description: true,
+          id: true,
+          title: true,
+          createdAt: true,
         },
-      ],
-      select: {
-        _count: true,
-        likedBy: true,
-        bookmarkedBy: true,
-        author: true,
-        description: true,
-        id: true,
-        title: true,
-        createdAt: true,
-      },
-    });
-    const posts = await Promise.all(
-      data.map(async (item) => {
-        const comments = await prisma.comment.findMany({
-          where: { postID: item.id },
-        });
+      });
+      const posts = await Promise.all(
+        data.map(async (item) => {
+          const comments = await prisma.comment.findMany({
+            where: { postID: item.id },
+          });
 
-        const followers = await prisma.user.findFirst({
-          where: { id: item.author.id },
-          select: { followedBy: true },
-        });
+          const followers = await prisma.user.findFirst({
+            where: { id: item.author.id },
+            select: { followedBy: true },
+          });
 
-        const following = followers?.followedBy.some(
-          (follower) => follower.id === clerkUser.id
-        );
+          const following = followers?.followedBy.some(
+            (follower) => follower.id === id
+          );
 
-        return {
-          postID: item.id,
-          author: item.author,
-          title: item.title,
-          description: item.description,
-          createdAt: item.createdAt,
-          likeCount: item._count.likedBy,
-          likedBy: item.likedBy,
-          bookmarkCount: item._count.bookmarkedBy,
-          bookmarkedBy: item.bookmarkedBy,
-          comments: comments,
-          following: following,
-        };
-      })
-    );
+          return {
+            postID: item.id,
+            author: item.author,
+            title: item.title,
+            description: item.description,
+            createdAt: item.createdAt,
+            likeCount: item._count.likedBy,
+            likedBy: item.likedBy,
+            bookmarkCount: item._count.bookmarkedBy,
+            bookmarkedBy: item.bookmarkedBy,
+            comments: comments,
+            following: following,
+          };
+        })
+      );
 
-    return {
-      message: "Fetch posts succesful!",
-      valid: true,
-      data: posts,
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      message: "Some unknown error occured!",
-      valid: false,
-    };
+      return {
+        message: "Fetch posts succesful!",
+        valid: true,
+        data: posts,
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        message: "Some unknown error occured!",
+        valid: false,
+      };
+    }
+  },
+  ["recent"],
+  {
+    tags: ["posts"],
   }
-};
+);
+
+export const getFollowingPosts = unstable_cache(
+  async ({ id }: { id: string }): Promise<RecentPostsReturn> => {
+    try {
+      const limit = 10;
+      const data = await prisma.post.findMany({
+        take: limit + 1,
+        where: {
+          author: {
+            followedBy: {
+              some: {
+                id: id,
+              },
+            },
+          },
+        },
+        orderBy: [
+          {
+            createdAt: "desc",
+          },
+        ],
+        select: {
+          _count: true,
+          likedBy: true,
+          bookmarkedBy: true,
+          author: true,
+          description: true,
+          id: true,
+          title: true,
+          createdAt: true,
+        },
+      });
+      const posts = await Promise.all(
+        data.map(async (item) => {
+          const comments = await prisma.comment.findMany({
+            where: { postID: item.id },
+          });
+
+          const followers = await prisma.user.findFirst({
+            where: { id: item.author.id },
+            select: { followedBy: true },
+          });
+
+          const following = followers?.followedBy.some(
+            (follower) => follower.id === id
+          );
+
+          return {
+            postID: item.id,
+            author: item.author,
+            title: item.title,
+            description: item.description,
+            createdAt: item.createdAt,
+            likeCount: item._count.likedBy,
+            likedBy: item.likedBy,
+            bookmarkCount: item._count.bookmarkedBy,
+            bookmarkedBy: item.bookmarkedBy,
+            comments: comments,
+            following: following,
+          };
+        })
+      );
+
+      return {
+        message: "Fetch posts succesful!",
+        valid: true,
+        data: posts,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        message: "Some unknown error occured!",
+        valid: false,
+      };
+    }
+  },
+  ["following"],
+  {
+    tags: ["posts"],
+  }
+);
+
+export const getBookmarkedPosts = unstable_cache(
+  async ({ id }: { id: string }): Promise<RecentPostsReturn> => {
+    try {
+      const limit = 10;
+      const data = await prisma.post.findMany({
+        take: limit + 1,
+        where: {
+          bookmarkedBy: {
+            some: {
+              id: id,
+            },
+          },
+        },
+        orderBy: [
+          {
+            createdAt: "desc",
+          },
+        ],
+        select: {
+          _count: true,
+          likedBy: true,
+          bookmarkedBy: true,
+          author: true,
+          description: true,
+          id: true,
+          title: true,
+          createdAt: true,
+        },
+      });
+      const posts = await Promise.all(
+        data.map(async (item) => {
+          const comments = await prisma.comment.findMany({
+            where: { postID: item.id },
+          });
+
+          const followers = await prisma.user.findFirst({
+            where: { id: item.author.id },
+            select: { followedBy: true },
+          });
+
+          const following = followers?.followedBy.some(
+            (follower) => follower.id === id
+          );
+
+          return {
+            postID: item.id,
+            author: item.author,
+            title: item.title,
+            description: item.description,
+            createdAt: item.createdAt,
+            likeCount: item._count.likedBy,
+            likedBy: item.likedBy,
+            bookmarkCount: item._count.bookmarkedBy,
+            bookmarkedBy: item.bookmarkedBy,
+            comments: comments,
+            following: following,
+          };
+        })
+      );
+
+      return {
+        message: "Fetch posts succesful!",
+        valid: true,
+        data: posts,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        message: "Some unknown error occured!",
+        valid: false,
+      };
+    }
+  },
+  ["bookmarks"],
+  {
+    tags: ["posts"],
+  }
+);
