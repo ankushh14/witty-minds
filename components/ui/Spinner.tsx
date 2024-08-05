59974ac08ce27fb77733aa6f2cef97b2a8ffa5d4
ankushh14 <ankushshenoy97@gmail.com>
2024-08-05 13:33:59 +0530

    style : spinner external div removed

--- components/ui/Spinner.tsx
+++ components/ui/Spinner.tsx
@@ -1,14 +1,10 @@
 const Spinner = () => {
   return (
     <div
-      className="w-full h-full min-h-dvh flex justify-center items-center"
+      className={`w-8 h-8 rounded-full border-t-2 border-slate-500 animate-spin`}
       role="alert"
       aria-label="Loading"
-    >
-      <div
-        className={`w-8 h-8 rounded-full border-t-2 border-slate-500 animate-spin`}
-      ></div>
-    </div>
+    ></div>
   );
 };
 
