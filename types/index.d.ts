7ef57970b9e618f7901060bc971b7a6e9fc75c55
ankushh14 <ankushshenoy97@gmail.com>
2024-08-05 13:34:41 +0530

    feat : added required types

--- types/index.d.ts
+++ types/index.d.ts
@@ -9,7 +9,71 @@ declare type ActionsReturnType = {
   valid: boolean;
 };
 
+declare type AddCommentProps = {
+  userID: string;
+  postID: string;
+  content: string;
+  profile: string;
+};
+
+declare type AddCommentReturn = ActionsReturnType & {
+  data?: {
+    id: string;
+    userID: string;
+    postID: string;
+    content: string;
+    userProfile: string;
+    createdAt: Date;
+  };
+};
+
 declare type PostCardProps = Omit<Post, "updatedAt"> & {
   author: Omit<User, "description">;
   following?: boolean;
+  likeCount: number;
+  likedBy: {
+    id: string;
+    name: string | null;
+    email: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+  }[];
+  bookmarkCount: number;
+  bookmarkedBy: {
+    id: string;
+    name: string | null;
+    email: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+  }[];
+  comments: {
+    id: string;
+    userID: string;
+    postID: string;
+    content: string;
+    userProfile: string;
+    createdAt: Date;
+  }[];
+};
+
+declare type MutateLikeProps = {
+  postID: string;
+  addLike: boolean;
+  userID: string;
+};
+
+declare type UpdateBookmarkProps = {
+  postID: string;
+  addBookmark: boolean;
+  userID: string;
+};
+
+declare type AddFollowProps = {
+  fromUserID: string;
+  toUserID: string;
+};
+
+declare type RemoveFollowProps = {
+  fromUserID: string;
+  toUserID: string;
 };
