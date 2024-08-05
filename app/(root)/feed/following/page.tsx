ce3e4234ca0e830cb4749524819f28803c49c212
ankushh14 <ankushshenoy97@gmail.com>
2024-08-05 13:31:42 +0530

    feat : created following page

--- app/(root)/feed/following/page.tsx
+++ app/(root)/feed/following/page.tsx
@@ -1,5 +1,40 @@
-const Followingpage = () => {
-  return <div>Followingpage</div>;
+import { getFollowingPosts } from "@/actions/posts/getPosts.actions";
+import PostCard from "@/components/feed/PostCard";
+import { currentUser } from "@clerk/nextjs/server";
+
+const Followpage = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser?.id) {
+    return;
+  }
+  const posts = await getFollowingPosts({ id: clerkUser?.id });
+  return (
+    <div
+      className="w-full flex flex-col space-y-4 py-4 justify-center items-center"
+      aria-label="Posts of people you follow"
+    >
+      {posts.valid &&
+        posts.data?.map((post) => {
+          return (
+            <PostCard
+              author={post.author}
+              createdAt={post.createdAt}
+              description={post.description}
+              id={post.postID}
+              title={post.title}
+              key={post.postID}
+              userId={post.author.id}
+              following={post.following}
+              bookmarkCount={post.bookmarkCount}
+              bookmarkedBy={post.bookmarkedBy}
+              comments={post.comments}
+              likeCount={post.likeCount}
+              likedBy={post.likedBy}
+            />
+          );
+        })}
+    </div>
+  );
 };
 
-export default Followingpage;
+export default Followpage;
