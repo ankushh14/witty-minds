0a983f76a22b53d5705cbeab92dc02646a8b8034
ankushh14 <ankushshenoy97@gmail.com>
2024-08-06 19:04:48 +0530

    style : card width corrections

--- components/feed/PostCard.tsx
+++ components/feed/PostCard.tsx
@@ -59,7 +59,7 @@ const PostCard = (props: PostCardProps) => {
   };
 
   return (
-    <Card className={`w-[300px] md:w-[450px] scroll-mt-16`} id={props.id}>
+    <Card className={`w-full max-w-[450px] scroll-mt-16`} id={props.id}>
       <CardHeader className="flex flex-row justify-center items-center">
         <Link
           href={`/profile/${encodeURIComponent(props.author.id)}`}
