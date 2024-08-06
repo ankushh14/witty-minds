95ab5f677291ae21b5b792a520102d1b54eadfe7
ankushh14 <ankushshenoy97@gmail.com>
2024-08-06 17:09:40 +0530

    feat : posts page in user profile

---
+++ app/(root)/profile/[id]/posts/page.tsx
@@ -0,0 +1,28 @@
+import { getuserPosts } from "@/actions/users/profile.actions";
+import Profileposts from "@/components/profile/Profileposts";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function page({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: { post: string };
+}) {
+  const clerkUser = await currentUser();
+  if (!clerkUser?.id) {
+    throw new Error("Invalid user");
+  }
+  const posts = await getuserPosts({
+    id: params.id,
+    currentuserId: clerkUser.id,
+  });
+  if (!posts.valid) {
+    throw new Error(posts.message);
+  }
+  return (
+    <div className="w-full" aria-label="posts">
+      <Profileposts posts={posts.data!} focusId={searchParams.post} />
+    </div>
+  );
+}
---
+++ components/profile/Profileposts.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { useEffect } from "react";
+import PostCard from "../feed/PostCard";
+
+type Props = {
+  posts: {
+    postID: string;
+    author: {
+      id: string;
+      name: string | null;
+      email: string | null;
+      profile: string | null;
+      createdAt: Date;
+      updatedAt: Date;
+    };
+    title: string;
+    description: string;
+    createdAt: Date;
+    likeCount: number;
+    likedBy: {
+      id: string;
+      name: string | null;
+      email: string | null;
+      createdAt: Date;
+      updatedAt: Date;
+    }[];
+    bookmarkCount: number;
+    bookmarkedBy: {
+      id: string;
+      name: string | null;
+      email: string | null;
+      createdAt: Date;
+      updatedAt: Date;
+    }[];
+    comments: {
+      id: string;
+      userID: string;
+      postID: string;
+      content: string;
+      userProfile: string;
+      createdAt: Date;
+    }[];
+    following?: boolean;
+  }[];
+  focusId?: string;
+};
+
+const Profileposts = ({ posts, focusId }: Props) => {
+  useEffect(() => {
+    if (focusId !== undefined && focusId) {
+      const focus = document.getElementById(focusId);
+      focus?.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div className="w-full flex flex-col justify-center items-center space-y-4 py-4">
+      {posts.length &&
+        posts.map((post) => {
+          return (
+            <PostCard
+              author={post.author}
+              createdAt={post.createdAt}
+              description={post.description}
+              id={post.postID}
+              title={post.title}
+              key={post.postID}
+              userId={post.author.id}
+              following={post.following}
+              bookmarkCount={post.bookmarkCount}
+              bookmarkedBy={post.bookmarkedBy}
+              comments={post.comments}
+              likeCount={post.likeCount}
+              likedBy={post.likedBy}
+            />
+          );
+        })}
+    </div>
+  );
+};
+
+export default Profileposts;
